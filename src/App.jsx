81c7712d0aa958343f87f004c794c8472cf0126b
azizt3 <104986484+azizt3@users.noreply.github.com>
2023-09-19 17:05:33 -0400

    1 file modified, 1 added

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,34 @@ import './App.css'
 
 function Weapon(props){
   return(
-    <button> {props.weapon} </button>
+    <button onClick = {props.onClickFunction}> {props.weapon} </button>
   )
 }
 
+function GameStatus(props) {
+  return(
+    <h2> {props.message} </h2>
+  )
+}
+
+function ScoreBoard(){
+}
+
 function App() {
+
+  const[counter, setCounter] = useState(0);
+  const roundCounter = () => setCounter(counter + 1 );
+
 	return (
     
     <div>
       <h1>Begin 5 rounds!</h1>
-      <Weapon weapon ='Rock'/>
-      <Weapon weapon ='Paper'/>
-      <Weapon weapon ='Scissors'/>
+      <Weapon onClickFunction = {roundCounter} weapon ='Rock'/>
+      <Weapon onClickFunction = {roundCounter} weapon ='Paper'/>
+      <Weapon onClickFunction = {roundCounter} weapon ='Scissors'/>
+      <GameStatus message = {counter}/>
+      <h2> Scoreboard: </h2>
+  
     </div>
   );
 }
