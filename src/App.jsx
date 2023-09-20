03ed9e0211656f04e288909abc8abd1fedea1280
azizt3 <104986484+azizt3@users.noreply.github.com>
2023-09-20 18:14:53 -0400

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,89 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
-
-function Weapon(props){
-  return(
-    <button onClick = {props.onClickFunction}> {props.weapon} </button>
-  )
+function Weapon(props) {
+  return <button onClick={props.onClickFunction}> {props.weapon} </button>;
 }
+
+//Will display the game status (Not Started, In progress, Completed); 
+//Will display the round #; ok
+//Will display who is winning; ok
+//Will display the record (W-L-T); ok
 
 function GameStatus(props) {
-  return(
-    <h2> {props.message} </h2>
-  )
-}
-
-function ScoreBoard(){
-  return(
-    <h2>0 - 0</h2>
-  )
-}
-
-function App() {
-
-  const[counter, setCounter] = useState(0);
-  const[computerWeapon, setComputerWeapon] = useState(0);
-  const roundCounter = () => setCounter(counter + 1 );
-
-	return (
-  
+  return (
     <div>
-      <h1>Begin 5 rounds!</h1>
-      <Weapon onClickFunction = {roundCounter} weapon ='Rock'/>
-      <Weapon onClickFunction = {roundCounter} weapon ='Paper'/>
-      <Weapon onClickFunction = {roundCounter} weapon ='Scissors'/>
-      <GameStatus message = {counter}/>
-      <ScoreBoard/>
-  
+      <h2> Status: {props.status} </h2>
+      <h2> Round: {props.round}</h2>
+      <h2> Winner: {props.winStatus}</h2>
+      <h2> My Record (W-L): {props.scoreBoard}</h2>
     </div>
   );
 }
 
+/*function ScoreBoard() {
+  return <h2>0 - 0</h2>;
+} 
+*/
 
-export default App
+
+
+
+
+
+function App() {
+  //OBJECTS
+
+  const Status = ['Not Started', 'In Progress', 'Complete'];
+  
+  const winLoseColors = {
+    winning: 'green',
+    losing: 'red'
+  }
+  
+  
+  //FUNCTIONS
+
+  function currentStatus() {
+    if (counter === 0) {
+      return setGameStatus(Status[0]);
+    } else counter > 0;
+    {
+      return setGameStatus(Status[1]);
+    }
+  }
+
+  function whosWinning() {
+    return playerScore > computerScore ? 'You are currently winning!' : 'You are losing!'
+  }
+
+ 
+
+  //STATES
+  const [counter, setCounter] = useState(0);
+  const [computerWeapon, setComputerWeapon] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const [playerScore, setPlayerScore] = useState(1);
+  const [gameStatus, setGameStatus] = useState('Not Started');
+
+  const roundCounter = () => setCounter(counter + 1);
+
+  return (
+    <div>
+      <h1>Begin 5 rounds!</h1>
+      <Weapon onClickFunction={roundCounter} weapon="Rock" />
+      <Weapon onClickFunction={roundCounter} weapon="Paper" />
+      <Weapon onClickFunction={roundCounter} weapon="Scissors" />
+      <GameStatus 
+        status={gameStatus}
+        round={counter}
+        winStatus ={whosWinning()}
+        scoreBoard = {playerScore+ " - " + computerScore} 
+
+      
+        />
+    </div>
+  );
+}
+
+export default App;
