c2b85a2d044ae54a3faae2dd88ea6193ee31ffc9
azizt3 <104986484+azizt3@users.noreply.github.com>
2023-09-21 12:23:04 -0400

    3 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import getComputerChoice from './common/rpslogic.js';
 
 function Weapon(props) {
   return <button onClick={props.onClickFunction}> {props.weapon} </button>;
@@ -10,21 +11,16 @@ function Weapon(props) {
 //Will display who is winning; ok
 //Will display the record (W-L-T); ok
 
-function GameStatus(props) {
+function GameDisplay(props) {
   return (
-    <div >
+    <div>
       <h2> Status: {props.status} </h2>
       <h2> Round: {props.round}</h2>
-      <h2 >Winner: {props.winStatus}</h2>
+      <h2>Winner: {props.winStatus}</h2>
       <h2> My Record (W-L): {props.scoreBoard}</h2>
     </div>
   );
 }
-
-/*function ScoreBoard() {
-  return <h2>0 - 0</h2>;
-} 
-*/
 
 function App() {
   //OBJECTS
@@ -36,18 +32,27 @@ function App() {
     losing: 'red',
   };
 
-
   //FUNCTIONS
+
+  const handleClick = (userChoice) => {
+
+    setCounter(counter + 1);
+    setComputerWeapon(() => getComputerChoice());
+
+    if (gameStatus === 'Not Started') {
+      setGameStatus('In Progress');
+    }
+  }
 
   function currentStatus() {
     if (counter === 0) {
       setGameStatus(Status[0]);
-    } if (counter > 0)
-    {
+    }
+    if (counter > 0) {
       setGameStatus(Status[1]);
     }
-    return gameStatus
-  };
+    return gameStatus;
+  }
 
   function whosWinning() {
     return playerScore > computerScore
@@ -55,26 +60,24 @@ function App() {
       : 'You are losing!';
   }
 
-
-
   //STATES
-  const [counter, setCounter] = useState(0);
-  const [computerWeapon, setComputerWeapon] = useState(0);
+  const [counter, setCounter] = useState(1);
+  const [computerWeapon, setComputerWeapon] = useState('');
   const [computerScore, setComputerScore] = useState(0);
   const [playerScore, setPlayerScore] = useState(1);
   const [gameStatus, setGameStatus] = useState('Not Started');
   const [currentWinner, setCurrentWinner] = useState('');
 
-  const roundCounter = () => setCounter(counter + 1);
-
   return (
     <div>
       <h1>Begin 5 rounds!</h1>
-      <Weapon onClickFunction={roundCounter} weapon="Rock" />
-      <Weapon onClickFunction={roundCounter} weapon="Paper" />
-      <Weapon onClickFunction={roundCounter} weapon="Scissors" />
-      
-      <GameStatus
+      <h2>Computer chose: {computerWeapon}</h2>
+      <Weapon onClickFunction={handleClick} weapon="Rock" />
+      <Weapon onClickFunction={handleClick} weapon="Paper" />
+      <Weapon onClickFunction={handleClick} weapon="Scissors" />
+
+      <GameDisplay
+        opponentWeapon={computerWeapon}
         status={gameStatus}
         round={counter}
         winStatus={whosWinning()}
